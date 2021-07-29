ac88f26f9d355857cb4ae8a9acabbe9926fdfc63
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-07-29 11:05:36 -0500

    adds updated function exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,33 @@
+USE codeup_test_db;
+
+USE employees;
+
+
+
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E'
+ORDER BY emp_no;
+
+
+SELECT *
+FROM employees
+WHERE MONTH(birth_date) = 12
+  AND DAY(birth_date) = 25;
+
+SELECT *, datediff(now(), hire_date)
+FROM employees
+WHERE MONTH(birth_date) = 12
+  AND DAY(birth_date) = 25
+  AND YEAR(hire_date) BETWEEN 1990 AND 1999
+ORDER BY birth_date, hire_date DESC;
+
+
+
+
+
+
+
+
+
